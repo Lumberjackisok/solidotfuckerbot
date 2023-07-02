207b4235e4c44d09f8f53c6cd1c94b59f22f795a
DrunkToBootup <inmymind1593@gmail.com>
2023-07-02 16:46:31 +0800

    add #

--- rssForwarder.js
+++ rssForwarder.js
@@ -65,7 +65,7 @@ async function forwardLatestRSS() {
 
           const formattedDate = moment(isoDate).utcOffset(8).format('YYYY-MM-DD HH:mm:ss');
           // 组合消息文本
-          const messageText = `<a href="${link}"><b>${title}</b></a>\n\n${formattedDate} by ${via}\n\n${content}\n\n${url}\n\n${tag}`;
+          const messageText = `<a href="${link}"><b>${title}</b></a>\n\n${formattedDate} by ${via}\n\n${content}\n\n${url}\n\n#${tag}`;
           // console.log('messageText:', messageText);
           const disablePreview = { disable_web_page_preview: true };
 
